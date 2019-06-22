798520cd16409c82bc9fc3add6217cee836cabca
Trey Shugart <treshugart@gmail.com>
2019-06-22 19:38:12 +1000

    Add ability to run npm scripts, if they exists but doesn't error if they don't.

--- bin.js
+++ bin.js
@@ -3,6 +3,7 @@
 const chalk = require("chalk");
 const cli = require("commander");
 const concurrently = require("concurrently");
+const path = require("path");
 const { findWorkspaces } = require(".");
 
 const colors = [
@@ -28,6 +29,26 @@ function randomColor() {
   return colors[Math.floor(Math.random() * colors.length - 1) + 1];
 }
 
+async function run(cmd, workspaces) {
+  const cmds = [];
+  for (const ws of workspaces) {
+    cmds.push({
+      // The "echo" is so that the command we run doesn't hang if listening
+      // for stdin.
+      command: `cd ${ws} && echo "" | ${cmd}`,
+      name: ws,
+      prefixColor: `${randomColor()}.inverse.bold`
+    });
+  }
+  if (cmds.length) {
+    try {
+      await concurrently(cmds);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+
 cli.option(
   "-w, --workspaces <glob>",
   "Filters or finds workspaces that match the specified pattern."
@@ -44,19 +65,30 @@ cli
   .command("run <cmd> [args...]")
   .description("Runs <cmd> in each workspace and passes in <options>.")
   .action(async (cmd, args) => {
-    const cmds = [];
+    await run(`${cmd} ${args.join(" ")}`, await findWorkspaces(cli.workspaces));
+  });
+cli
+  .command("npm <cmd> [args...]")
+  .description("Runs the specified npm commands, if they exist.")
+  .action(async (cmd, args) => {
+    const filteredWorkspaces = [];
+
     for (const ws of await findWorkspaces(cli.workspaces)) {
-      cmds.push({
-        // The "echo" is so that the command we run doesn't hang if listening
-        // for stdin.
-        command: `cd ${ws} && echo "" | ${cmd} ${args.join(" ")}`,
-        name: ws,
-        prefixColor: `${randomColor()}.inverse.bold`
-      });
+      let pkg;
+      try {
+        pkg = require(path.resolve(ws, "package.json"));
+      } catch (e) {
+        continue;
+      }
+
+      if (!pkg.scripts || !pkg.scripts[cmd]) {
+        continue;
+      }
+
+      filteredWorkspaces.push(ws);
     }
-    if (cmds.length) {
-      concurrently(cmds);
-    }
+
+    await run(`npm run ${cmd} ${args.join(" ")}`, filteredWorkspaces);
   });
 
 cli.parse(process.argv);
